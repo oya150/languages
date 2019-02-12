3d0e540b6184bbcc424c3883ed049c2c5a1b50ac
tonny.k <tonny.k@kakaocorp.com>
2019-02-12 16:44:34 +0900

    반복문

--- tutorial.py
+++ tutorial.py
@@ -13,5 +13,26 @@ print(name * 2) #KimSungHyunKimSungHyun
 print("=" * 10) #==========
 
 
-#숫자 문자열 변환
+##반복문 조건문
+mihs = ['m','a','d','e','i','n','h','e','a','v','e','n']
+alphabets = [chr(i) for i in range(97,123)]
+print(alphabets)
+
+#정렬
+
+print(mihs)
+      
+'''
+maxAlphabet = ''
+maxAlphabetCnt = 0
+mihCnt = 0
+for mih in mihs:
+    print(alphabet)
+    mihCnt = 0
+    for alphabet in alphabets:
+        if mih==alphabet:
+'''
+
+#정렬 라이브러리 사용
+mihs.sort() #timesort algorithm
 
