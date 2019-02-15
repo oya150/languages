8093770e75cba5c0126d3ea74980535db729c1dd
tonny.k <tonny.k@kakaocorp.com>
2019-02-15 17:24:41 +0900

    구간별 최대 합계 구하기

--- tutorial.py
+++ tutorial.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*- 
 #출력
-print("Hello World!")
+#print("Hello World!")
 
 #변수 출력
 num = 1
 name = "KimSungHyun"
-print(num)
-print(name)
+#print(num)
+#print(name)
 
 #문자열 반복
-print(name * 2) #결과: KimSungHyunKimSungHyun
-print("=" * 10) #결과: ==========
+#print(name * 2) #결과: KimSungHyunKimSungHyun
+#print("=" * 10) #결과: ==========
 
 
 ##반복문 조건문
 ##주어진 문자열에서 가장 많은 빈도수를 가진 문자 출력하기 
 mihs = ['m','a','d','e','i','n','h','e','a','v','e','n']
 alphabets = [chr(i) for i in range(97,123)]
-print(alphabets)
+#print(alphabets)
 
 '''
 #정렬 라이브러리 사용
@@ -50,7 +50,7 @@ dicMih = {}
 for mih in mihs:
     dicMih[mih] = 0
 
-print(dicMih)
+#print(dicMih)
 
 mihCnt = 0
 maxMihCnt = 0
@@ -63,8 +63,8 @@ for mih in mihs:
         mihCnt = dicMih[mih]
         maxMih = mih
 
-print(dicMih)
-print(maxMih)
+#print(dicMih)
+#print(maxMih)
 
 '''
 - 반복문(range, 증가,감소), 배열사이즈
@@ -76,28 +76,43 @@ scores = [3,2,4,6,2,1,3,5,4,8,4,3]
 averSize = 3
 scoreSize = len(scores)
 startNum = averSize-1
-'''
+
+#중복루프 이용 
 for i in range(startNum, scoreSize):
     average = 0
     sumVal = 0
     for j in range(i,i-averSize, -1):
         sumVal += scores[j]
         
-    print(str(i+1) + ' 월평균 :: ' + str(sumVal/averSize))
-'''
+    #print(str(i+1) + ' 월평균 :: ' + str(sumVal/averSize))
 
+
+#단일루프 이용 
 sumScore = 0
 for i in range(0, averSize):
-    print(scores[i])
+    #print(scores[i])
     sumScore += scores[i]
-print(sumScore)
+    #print(sumScore)
 
 firstNum = 0
 for j in range(startNum, scoreSize, 1):
-    print(str(j+1) + ' 월평균 :: ' + str(sumScore/averSize))
+    #print(str(j+1) + ' 월평균 :: ' + str(sumScore/averSize))
     if j != scoreSize-1:
         sumScore -= scores[firstNum]
         sumScore += scores[j+1]
         firstNum += 1
 
-    
+
+numbers = [-7,4,-3,6,3,-8,3,4,]
+
+numberSize = len(numbers)
+maxSum = 0
+for i in range(0,numberSize):
+    for j in range(i,numberSize):
+        pSum = 0
+        for k in range(i,j):
+            pSum += numbers[k]
+        if pSum > maxSum:
+            maxSum = pSum
+print(maxSum)
+
